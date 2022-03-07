80bd6da022ead8b740201f9b22d5d4b3a853f4b2
Darren Southwell <darren.southwell@unimelb.edu.au>
2022-03-07 23:20:48 +1100

    Move disease spread dynamics to front of code

--- R/population_dispersal-functions.R
+++ R/population_dispersal-functions.R
@@ -386,7 +386,10 @@ cellular_automata_dispersal <- function (mean_cells = Inf,
                                          dispersal_proportion = set_proportion_dispersing(),
                                          barriers = NULL,
                                          use_suitability = TRUE,
-                                         carrying_capacity = "carrying_capacity") {
+                                         carrying_capacity = "carrying_capacity",
+                                         barrier_breach_probability = NULL,
+                                         DFT1_annual_spread,
+                                         DFT2_annual_spread) {
   
   # are there suitability and carrying_capacity landscape objects specified?
   carrying_cap <- identical(carrying_capacity, "carrying_capacity")
@@ -576,8 +579,8 @@ cellular_automata_dispersal <- function (mean_cells = Inf,
     
     #Now run diffusion of DFT1 and DFT2 after 9 years and 30 years respectively. The argument "1" means that cells occupied by all age classes
     #can spread DFT, whereas a value of "2" means that only cells with some devils >1 year old spread DFT
-    if (timestep < 9) {out <- DFT1_raster} else {out <- DFT_diffusion_iterative(DFT1_raster, age_class_matrix, density_raster_scaled, dist_5, 4, barrier_matrix, 0.33)}   #-------------------------->>>>>>MOVE NEIGHBOURHOOD TO FRONT OF CODE?
-    if (timestep < 30) {out3 <- DFT2_raster} else {out3 <- DFT_diffusion_iterative(DFT2_raster, age_class_matrix, density_raster_scaled, dist_5, 4, barrier_matrix, 0.33)}
+    if (timestep < 9) {out <- DFT1_raster} else {out <- DFT_diffusion_iterative(DFT1_raster, age_class_matrix, density_raster_scaled, dist_5, DFT1_annual_spread, barrier_matrix, barrier_breach_probability)}   #-------------------------->>>>>>MOVE NEIGHBOURHOOD TO FRONT OF CODE?
+    if (timestep < 30) {out3 <- DFT2_raster} else {out3 <- DFT_diffusion_iterative(DFT2_raster, age_class_matrix, density_raster_scaled, dist_5, DFT2_annual_spread, barrier_matrix, barrier_breach_probability)}
     
     #Now model spread of DFT1 through migration when individuals move and establish to neighbouring cells
     #In the diffusion model, cells with low densities have a low probability of being infected
