de625b60830a0c5aaed8bbe95a40bcaae49138e6
Darren Southwell <darren.southwell@unimelb.edu.au>
2022-04-05 16:10:40 +1000

    Last minute workshop testing

--- R/population_dispersal-functions.R
+++ R/population_dispersal-functions.R
@@ -632,7 +632,7 @@ cellular_automata_dispersal <- function (mean_cells = Inf,
     
     DFT1_raster_new <- DFT1_raster + out
     DFT1_raster_new[DFT1_raster_new > 1] <- 1
-    DFT1_raster_new[age_class_stack_all == 0] <- 0
+    #DFT1_raster_new[age_class_stack_all == 0] <- 0
     if (timestep %in% c(30:37)) {
       DFT1_raster_new[arrival_raster==100] <- 0
       DFT1_raster_new[landscape$barrier[[6]] == 1] <- 0
@@ -641,7 +641,7 @@ cellular_automata_dispersal <- function (mean_cells = Inf,
     
     DFT2_raster_new <- DFT2_raster + out3
     DFT2_raster_new[DFT2_raster_new > 1] <- 1
-    DFT2_raster_new[age_class_stack_all == 0] <- 0
+    #DFT2_raster_new[age_class_stack_all == 0] <- 0
     landscape$DFTD2 <-  DFT2_raster_new
     
     ######################################################################
